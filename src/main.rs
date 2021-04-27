9e1d60df77f8eebae33616c213b14a68aacb9dfe
yzia2000 <e0381439@u.nus.edu>
2021-04-27 23:20:41 +0800

    Added sqlparser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use sqlparser::dialect::GenericDialect;
+use sqlparser::parser::Parser;
+
+pub fn main() {
+    let dialect = GenericDialect {}; // or AnsiDialect
+
+    let sql = "SELECT a, b, 123, myfunc(b) \
+           FROM table_1 \
+           WHERE a > b AND b < 100 \
+           ORDER BY a DESC, b";
+
+    let ast = Parser::parse_sql(&dialect, &sql.to_string()).unwrap();
+
+    println!("AST: {:?}", ast);
 }
